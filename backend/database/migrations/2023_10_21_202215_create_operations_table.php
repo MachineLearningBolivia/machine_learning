5c5b209e9f1db34c0d29d6931c5ad71c56f39255
maccXX98 <dahakar1998@gmail.com>
2023-10-21 17:34:30 -0400

    first commit

---
+++ backend/database/migrations/2023_10_21_202215_create_operations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('operations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('slug');
+            $table->foreignId('operation_type_id')->constrained('operation_types');
+            $table->foreignId('box_id')->constrained('boxes');
+            $table->foreignId('user')->constrained('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('operations');
+    }
+};
