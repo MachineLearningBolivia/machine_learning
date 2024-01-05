cf883747b4bca705c8e5738ff8a9bf96f16a737b
hunk12369 <kevinbryanmora2@hotmail.com>
2024-01-05 09:56:51 -0400

    feat: request box and category

---
+++ frontend/api/box.js
@@ -0,0 +1,4 @@
+import axios from "./axios";
+
+export const getBox = () => axios.get("/boxes");
+//export const postProduct = (productData) => axios.post("/products", productData);
--- frontend/src/views/admin/Box.vue
+++ frontend/src/views/admin/Box.vue
@@ -6,7 +6,7 @@
       </div>
       <button-add to="/newBox"> Agregar Caja </button-add>
     </template>
-    <data-table :items="itemsDisplay" :columns="columnas" :options="options">
+    <data-table :items="itemsDisplay" :columns="columnas" :options=1 :modelValue="itemsDisplay">
     </data-table>
   </card-data>
 </template>
@@ -15,4 +15,39 @@ import CardData from "@/components/Cards/CardData.vue";
 import Search from "@/components/Inputs/Search.vue";
 import DataTable from "@/components/Tables/DataTable.vue";
 import ButtonAdd from "@/components/button/ButtonAdd.vue";
+
+import { getBox } from "../../../api/box.js"
+import { ref, onMounted } from "vue";
+
+const items = ref([]);
+const load = ref(true);
+const itemsDisplay = ref([]);
+
+const columnas = ref([
+  { key: "name", label: "Nombre" },
+  { key: "description", label: "Descripción" },
+]);
+
+async function loadData() {
+  load.value = true;
+  try {
+    const res = await getBox();
+    items.value = res.data;
+    console.log(res)
+    itemsDisplay.value = items.value.data;
+    load.value = false;
+    //console.log(items.value);
+    //console.log(items.value.data[3]);
+    //console.log(itemsDisplay.value[0]);
+    //console.log(items.value[0]);
+  } catch (error) {
+    toast.error("Error al cargar datos");
+  }
+}
+
+
+onMounted(() => {
+  loadData();
+});
+
 </script>
