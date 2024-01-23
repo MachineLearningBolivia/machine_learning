c28bc37a7b49ae5201385a76065f508cadc00596
mattahuasi <mattahuasi@gmail.com>
2024-01-23 11:03:10 -0400

    Products public

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ClientsProductsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -36,3 +37,5 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('logout', [AuthController::class, 'logout']);
 });
+
+Route::get('client/products', [ClientsProductsController::class, 'index']);
